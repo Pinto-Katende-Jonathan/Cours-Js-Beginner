1f98ccf1fb48e0717384586bcd9e933c4fd48b2f
Pinto-Katende-Jonathan <katendepinto@gmail.com>
2023-08-09 23:05:26 +0200

    Javascript function recapp

---
+++ main.js
@@ -0,0 +1,64 @@
+// function greeting(name){
+//     console.log('Hello '+name)
+// }
+
+// const greet = function (name){
+//     console.log('hello '+ name)
+//     return 5   
+// }
+
+// let age = greet('hello')
+
+// console.log(age)
+
+// const sum = function (a, b){
+//     if(typeof a === "number" && typeof b==="number"){
+//         return a+b
+//     }
+//     return;
+// }
+
+// console.log(sum(1,"kat"))
+
+// //Fonction flechée
+// const greet = name => {
+//     console.log('salut '+ name)
+// }
+
+// const somme = (a, b) => {
+//     return a+b
+// }
+
+// // greet('gloire')
+
+// const sum = somme(3,4)
+// console.log(sum)
+
+// function greet(){
+//     console.log(this)
+// }
+
+// const age = nbr => {
+//     console.log(nbr, this)
+// }
+
+// age(12)
+
+// const greet = nom =>  'hello ' + nom
+// console.log(greet('gard'))
+
+// function greet(nom = 'freddy'){
+//     console.log(nom)
+// }
+
+// greet()
+
+// const identite = {
+//     nom : 'gloire',
+//     age : 40,
+//     mineur : function mineurrr(n = this.age){
+//         return `${this.nom} a ${n} ans`
+//     }
+// }
+
+// console.log(identite.mineur(15))
